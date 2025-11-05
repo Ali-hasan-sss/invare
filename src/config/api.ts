c99a1c6c7142de019afa51e3a9fcd89eb3915e72
Ali hasan <160386537+Ali-hasan-sss@users.noreply.github.com>
2025-11-05 22:52:54 +0300

    add user board

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://72.61.112.229:3000",
+  BASE_URL: "https://api.invare.sa",
   ENDPOINTS: {
     AUTH: {
       LOGIN: "/auth/login",
